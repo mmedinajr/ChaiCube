fc338afd063c335dee9f8fb07c8f8283e3bd188c
Manuel Medina Jr <mmedina@ub.edu.bz>
2024-03-14 14:57:17 -0600

    Removed Chef Class

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 //Example 1 - Mocha & Chai Testing Cube Class
-/*
+
 class Cube {
     constructor(length) {
     this.length = length;
@@ -21,51 +21,3 @@ class Cube {
     module.exports = {
     Cube:Cube
     } 
-*/
-//Example 2 - Chef Example-------------------------------------------------------------------------------
-//Here, we have a Chef which can cook all the dishes mentioned in the dishes array.
-     var Chef = function(){
-        this.dishes = ['Dosa', 'Tea', 'Pokoras', 'Pav Bhaji'];
-        this.customers = 5; //add custmers
-    };
-    Chef.prototype.checkMenu= function(){
-        //returns a string
-        //returns a random dish from the dishes array
-        let dish = this.dishes[Math.floor(Math.random() * this.dishes.length)]; //To Fail add interger 3
-    
-        console.log("I will like to have:", dish);
-        return dish;
-    };
-    /*Added a new functionality to our chef, the number of customers he can cook for in a day. 
-    He can serve five customers in his restaurant, which is described in the customersFed function. 
-    We serve customers till they become more than five, and return a message accordingly when the chef can't cook anymore.*/
-
-    Chef.prototype.customersFed= function(){
-
-        if(this.customers >=1){
-    
-            console.log("Customer fed with yummy food!");
-            this.customers--;
-    
-        }
-        else if(this.customers == 0)
-        {
-            console.log("All customers have been satisfied. Done for the day! " + this.customers);
-        }
-        else
-        {
-            let cusLeft= this.customers;
-            console.log("Customer fed with yummy food!", cusLeft , "more to go!");
-            this.customers-= cusLeft;
-        }
-
-        
-        
-    
-        return this.customers;  
-    };
-
-
-    let chef = new Chef();
-    
-    module.exports = chef; 
--- test/app.test.js
+++ test/app.test.js
@@ -1,5 +1,5 @@
 //Test Case 1 - To test Cube Class
-/*const Cube = require('../src/app').Cube;
+const Cube = require('../src/app').Cube;
 const expect = require('chai').expect;
 
 describe('Testing the Cube Functions', function() {
@@ -22,37 +22,3 @@ done();
 });
 
 }); //end of Square Test
-
-*/
-
-/*------------------------------Test Case 2 - To test Chef Class-----------------------------------*/
-const Chef = require('../src/app');
-const assert = require('chai').assert;
-
-
-describe('Testing Chef Functions', function(){
-   
-    let chef = Chef;
-    
-    it('Check the dish has valid name.', function(){//to skip a test use it.skip
-        assert.isString(chef.checkMenu(), 'string');
-    })
-    
-    it('Check for a dish in menu.', function (){
-        let dish = chef.checkMenu();
-        assert.oneOf(dish, chef.dishes)
-
-    });
-    /*Here, we check if the chef can feed more customers by running the customersFed function five times 
-    and testing what happens when the customer count reaches 0. We do this by making use of the isAtLeast method, 
-    which takes in two parameters, the object to be tested, and its minimum value.*/
-    it('Make sure the Chef can feed more.', function(){
-
-        for(var i=0; i<6; i++){
-            chef.customersFed();
-            assert.isAtLeast(chef.customers, 0); 
-        }
-        
-    });
-    
-}); //End of Chef Test
